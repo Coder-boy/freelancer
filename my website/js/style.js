85b0c5d0e4828b80fe76dfc824d5eaaf66c9d6a2
ashrafahmed1995 <60040309+ashrafahmed1995@users.noreply.github.com>
2020-06-14 00:44:08 +0300

    Add files via upload
    a responsive website design by me

---
+++ my website/js/style.js
@@ -0,0 +1,27 @@
+
+document.querySelector(".menu-icon").addEventListener("click",function () {
+    document.querySelector("nav ul").classList.toggle("showing")
+    
+});
+
+
+ window.addEventListener("scroll",function () {
+     let MenuArea = document.getElementById('menu-area');
+
+    if (window.pageYOffset >0) {
+         MenuArea.classList.add("black");
+     } else {
+        MenuArea.classList.remove("black");
+     }
+     
+     
+     
+ });
+ 
+
+
+
+
+    
+
+
